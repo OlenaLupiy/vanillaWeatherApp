c9389a5885b6aca4ac6e2dcff02b79d7c389fd5d
OlenaLupiy <ollupiy@gmail.com>
2023-07-03 21:45:30 +0100

    Unit conversion

--- src/app.js
+++ src/app.js
@@ -22,7 +22,9 @@ function displayWeather(response){
     let dateItem = document.querySelector("#date");
     let iconItem = document.querySelector("#icon");
 
-    temperatureItem.innerHTML = Math.round(response.data.temperature.current);
+    celsiusTemperature = Math.round(response.data.temperature.current);
+
+    temperatureItem.innerHTML = Math.round(celsiusTemperature);
     cityItem.innerHTML = response.data.city;
     descriptionItem.innerHTML = response.data.condition.description;
     humidityItem.innerHTML = response.data.temperature.humidity;
@@ -35,8 +37,48 @@ function displayWeather(response){
     dateItem.innerHTML = formatTime(response.data.time *1000);
 
 }
-let city = "Deli"
-let apiKey = "bfot5b9073814ac4cad9fb1fee3aac2c";
+
+function search (city){
+    let apiKey = "bfot5b9073814ac4cad9fb1fee3aac2c";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event){
+event.preventDefault();
+let citySearchItem = document.querySelector("#city_search");
+search(citySearchItem.value);
+}
+
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", handleSubmit)
+
+let celsiusTemperature = null;
+
+
+function displayFahrenheitTemp(event){
+    event.preventDefault();
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let temperatureItem = document.querySelector("#temperature");
+    let tempFahrenheit = (celsiusTemperature*9)/5 + 32;
+    temperatureItem.innerHTML = Math.round(tempFahrenheit);
+}
+function displayCelsius(event){
+event.preventDefault();
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+let temperatureItem = document.querySelector("#temperature");
+temperatureItem.innerHTML = celsiusTemperature
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit_link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius_link");
+celsiusLink.addEventListener("click", displayCelsius);
+
+search("Kyiv");
+
