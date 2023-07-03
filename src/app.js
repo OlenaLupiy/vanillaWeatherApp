b13223da76e3409d1051d96591fd13e2bc8718f3
OlenaLupiy <ollupiy@gmail.com>
2023-07-03 18:43:16 +0100

    set icon

--- src/app.js
+++ src/app.js
@@ -20,17 +20,23 @@ function displayWeather(response){
     let humidityItem = document.querySelector("#humidity");
     let windItem = document.querySelector("#wind");
     let dateItem = document.querySelector("#date");
+    let iconItem = document.querySelector("#icon");
 
     temperatureItem.innerHTML = Math.round(response.data.temperature.current);
     cityItem.innerHTML = response.data.city;
     descriptionItem.innerHTML = response.data.condition.description;
     humidityItem.innerHTML = response.data.temperature.humidity;
     windItem.innerHTML = Math.round(response.data.wind.speed);
+    iconItem.setAttribute(
+      "src",
+      `${response.data.condition.icon_url}`
+    );
+    iconItem.setAttribute("alt", response.data.condition.description);
     dateItem.innerHTML = formatTime(response.data.time *1000);
 
 }
-
+let city = "Deli"
 let apiKey = "bfot5b9073814ac4cad9fb1fee3aac2c";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
